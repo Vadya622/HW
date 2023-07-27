dea6f83502f080fe5796543bc5820d6f8a6b5cd9
Vadim <dubovoy12344@gmail.com>
2023-07-27 04:11:19 +0300

    первый

---
+++ HW_2_1/HW_2_1/Result .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1
+
+{
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+ 
+
+
+}
