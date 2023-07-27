5c2898c5efe83bbf06a7cd1bee609b0c0a5f86b3
Vadim <dubovoy12344@gmail.com>
2023-07-27 04:11:19 +0300

    первый

---
+++ HW_2_1/HW_2_1/Actions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1
+{
+    public class Actions
+    {
+        private Logger logger;
+
+        public Actions()
+        {
+            logger = Logger.Instance;
+        }
+
+        public Result Method1()
+        {
+            logger.LogInfo("Метод запуска: Method1");
+            return new Result { Status = true };
+        }
+
+        public Result Method2()
+        {
+            logger.LogWarning("Пропущенная логика в методе: Method2");
+            return new Result { Status = true };
+        }
+
+        public Result Method3()
+        {
+            logger.LogError("Я нарушил логику");
+            return new Result { Status = false, ErrorMessage = "Я нарушил логику" };
+        }
+    }
+
+}
---
+++ HW_2_1/HW_2_1/Run.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Starter starter = new Starter();
+            starter.Run();
+        }
+    }
+
+}
