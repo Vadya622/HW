c29bd9ab155bdac2028618163e2f90bd1450fd44
Vadim <dubovoy12344@gmail.com>
2023-10-07 17:45:38 +0300

    1 шаг

--- HW_3_5/HW_3_5/Program.cs
+++ HW_3_5/HW_3_5/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace HW_3_5
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        string result = await ConcatenateAsync();
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> ReadFileAsync(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName))
         {
-            Console.WriteLine("Hello, World!");
+            return await reader.ReadToEndAsync();
         }
+    }
+
+    static async Task<string> ConcatenateAsync()
+    {
+        Task<string> helloTask = ReadFileAsync("Hello.txt");
+        Task<string> worldTask = ReadFileAsync("World.txt");
+
+        await Task.WhenAll(helloTask, worldTask);
+
+        string helloText = await helloTask;
+        string worldText = await worldTask;
+
+        return helloText + " " + worldText;
     }
 }
