19ab146eecd25017e2f7ffecd7b5de343208fc9f
Vadim <dubovoy12344@gmail.com>
2023-10-12 22:30:43 +0300

    2

--- HW_3_6/HW_3_6/Program.cs
+++ HW_3_6/HW_3_6/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace HW_3_6
+﻿using System;
+using System.Threading.Tasks;
+
+public class MessageBox
 {
-    internal class Program
+    public enum State
     {
-        static void Main(string[] args)
+        Хорошо,
+        Отмена
+    }
+
+    public event Action<State> WindowClosed;
+
+    public async void Open()
+    {
+        Console.WriteLine("Окно открыто.");
+        await Task.Delay(3000); // Подождем 3 секунды
+        Console.WriteLine("Окно было закрыто пользователем.");
+        // Генерируем случайное состояние
+        Random random = new Random();
+        State randomState = (random.Next(2) == 0) ? State.Хорошо : State.Отмена;
+        WindowClosed?.Invoke(randomState); // Вызываем событие закрытия окна
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MessageBox messageBox = new MessageBox();
+        messageBox.WindowClosed += OnWindowClosed;
+        messageBox.Open();
+    }
+
+    static void OnWindowClosed(MessageBox.State state)
+    {
+        if (state == MessageBox.State.Хорошо)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Операция подтверждена.");
+        }
+        else
+        {
+            Console.WriteLine("Операция отклонена.");
         }
     }
 }
