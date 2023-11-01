01bb1510e42fef2eac44b4e92f5f891f21feb0b7
Vadim <dubovoy12344@gmail.com>
2023-11-01 19:25:04 +0200

    2

--- HW_3_3/HW_3_3/Program.cs
+++ HW_3_3/HW_3_3/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace HW_3_3
+﻿using System;
+
+// Класс 1
+public class Class1
 {
-    internal class Program
+    public delegate void ShowDelegate(bool value);
+    public delegate int MultiplyDelegate(int a, int b);
+
+    public void Show(bool value)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine($"The value is: {value}");
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+// Класс 2
+public class Class2
+{
+    public Class1.ShowDelegate Calc(Class1.MultiplyDelegate del, int a, int b)
+    {
+        int result = del(a, b);
+        return ShowResult;
+    }
+
+    public void ShowResult(bool result)
+    {
+        Console.WriteLine($"The result is: {result}");
+    }
+}
+
+// Класс Program
+class Program
+{
+    static void Main(string[] args)
+    {
+        Class1 class1 = new Class1();
+        Class2 class2 = new Class2();
+
+        Class1.ShowDelegate showDelegate = new Class1.ShowDelegate(class1.Show);
+
+        int a = 5;
+        int b = 3;
+
+        Class1.MultiplyDelegate multiplyDelegate = new Class1.MultiplyDelegate(class1.Multiply);
+        var calcDelegate = class2.Calc(multiplyDelegate, a, b);
+
+        calcDelegate(true);
     }
 }
